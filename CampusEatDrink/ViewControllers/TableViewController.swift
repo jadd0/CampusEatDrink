51ac8f81768872cf560ea57aee5eff04d1af1093
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2024-12-02 14:41:21 +0000

    forgot to stage on last commit. same as prior message

--- CampusEatDrink/ViewControllers/TableViewController.swift
+++ CampusEatDrink/ViewControllers/TableViewController.swift
@@ -25,7 +25,25 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
         return cell
     }
 
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Perform the segue when a cell is selected
+        performSegue(withIdentifier: "ShowVenueDetail", sender: self)
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Check the segue identifier
+        if segue.identifier == "ShowVenueDetail" {
+            // Get the selected row
+            if let indexPath = locationsTable.indexPathForSelectedRow {
+                // Get the destination view controller
+                if let destinationVC = segue.destination as? VenueViewController {
+                    // Pass the selected venue to the destination view controller
+                    destinationVC.selectedVenue = globalFoodData?.food_venues[indexPath.row]
+                }
+            }
+        }
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- CampusEatDrink/ViewControllers/VenueViewController.swift
+++ CampusEatDrink/ViewControllers/VenueViewController.swift
@@ -8,22 +8,19 @@
 import UIKit
 
 class VenueViewController: UIViewController {
-
+    @IBOutlet weak var venueTitle: UILabel!
+    @IBOutlet weak var venueDescription: UILabel!
+    
+    var selectedVenue: Venue_Info?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let venue = selectedVenue {
+            venueTitle.text = venue.name
+            venueDescription.text = venue.description
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
