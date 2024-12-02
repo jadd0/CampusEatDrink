4655b379a72a7f1c8b737c4ec3d848a93bdcbf21
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2024-12-02 20:06:50 +0000

    Changed so table's values are sorted by distance to location

--- CampusEatDrink/ViewControllers/TableViewController.swift
+++ CampusEatDrink/ViewControllers/TableViewController.swift
@@ -6,50 +6,77 @@
 //
 
 import UIKit
+import CoreLocation
 
-class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, CLLocationManagerDelegate {
     @IBOutlet weak var locationsTable: UITableView!
     
+    var locationManager = CLLocationManager()
+    var sortedVenues: [Venue_Info] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let userLocation = locations.last else { return }
+        
+        // Stop updating location after getting the first location
+        locationManager.stopUpdatingLocation()
+        
+        // Sort venues by distance
+        sortedVenues = (globalFoodData?.food_venues.sorted { venue1, venue2 in
+            guard let lon1 = Double(venue1.lon),
+                  let lat1 = Double(venue1.lat),
+                  let lat2 = Double(venue2.lat),
+                  let lon2 = Double(venue2.lon) else {
+                return false
+            }
+            
+            let venue1Location = CLLocation(latitude: lat1, longitude: lon1)
+            let venue2Location = CLLocation(latitude: lat2, longitude: lon2)
+            
+            let distance1 = userLocation.distance(from: venue1Location)
+            let distance2 = userLocation.distance(from: venue2Location)
+            
+            return distance1 < distance2
+        })!
+        
+        // Reload table with sorted venues
+        locationsTable.reloadData()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return globalFoodData?.food_venues.count ?? 0
+        return sortedVenues.count
     } // TODO: fall back on core data
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "locationsTableCell", for: indexPath)
         var content = UIListContentConfiguration.subtitleCell()
 
-        content.text = globalFoodData?.food_venues[indexPath.row].name
-        content.secondaryText = globalFoodData?.food_venues[indexPath.row].building
+        let venue = sortedVenues[indexPath.row]
+        content.text = venue.name
+        content.secondaryText = venue.building
         
         cell.contentConfiguration = content
         return cell
     }
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // Perform the segue when a cell is selected
         performSegue(withIdentifier: "ShowVenue", sender: self)
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Check the segue identifier
         if segue.identifier == "ShowVenue" {
-            // Get the selected row
-            if let indexPath = locationsTable.indexPathForSelectedRow {
-                // Get the destination view controller
-                if let destinationVC = segue.destination as? VenueViewController {
-                    // Pass the selected venue to the destination view controller
-                    destinationVC.selectedVenue = globalFoodData?.food_venues[indexPath.row]
-                }
+            if let indexPath = locationsTable.indexPathForSelectedRow,
+               let destinationVC = segue.destination as? VenueViewController {
+                destinationVC.selectedVenue = sortedVenues[indexPath.row]
             }
         }
     }
-
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-    
 }
