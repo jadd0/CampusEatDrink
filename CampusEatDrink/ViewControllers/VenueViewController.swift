d791cf38df1dd2bcfac9e8030192c62e1d7266dd
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2024-12-02 14:40:50 +0000

    new view controller for the selection of a venue

---
+++ CampusEatDrink/ViewControllers/VenueViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VenueViewController.swift
+//  CampusEatDrink
+//
+//  Created by Jadd Al-Khabbaz on 02/12/2024.
+//
+
+import UIKit
+
+class VenueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
