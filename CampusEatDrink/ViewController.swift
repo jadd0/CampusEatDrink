394680fdf6e7a014ba4262b6bc58a7582aac7631
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2024-12-02 13:19:34 +0000

    Now all the comments are how I want them

--- CampusEatDrink/ViewController.swift
+++ CampusEatDrink/ViewController.swift
@@ -10,7 +10,7 @@ import MapKit
 import CoreLocation
 
 
-class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate, MKMapViewDelegate, CLLocationManagerDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     @IBOutlet weak var campusMap: MKMapView!
     
     var locationManager = CLLocationManager()
@@ -18,28 +18,30 @@ class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegat
     var startTrackingTheUser = false
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let locationOfUser = locations[0] //this method returns an array of locations
-        //generally we always want the first one (usually there's only 1 anyway)
+        let locationOfUser = locations[0]
+        
         let latitude = locationOfUser.coordinate.latitude
         let longitude = locationOfUser.coordinate.longitude
-        //get the users location (latitude & longitude)
+        
+        // Get the users location (latitude & longitude)
         let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
         if firstRun {
             firstRun = false
+            
             let latDelta: CLLocationDegrees = 0.0025
             let lonDelta: CLLocationDegrees = 0.0025
-            //a span defines how large an area is depicted on the map.
+            
+            // A span defines how large an area is depicted on the map
             let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
-            //a region defines a centre and a size of area covered.
+            
+            // A region defines a centre and a size of area covered
             let region = MKCoordinateRegion(center: location, span: span)
-            //make the map show that region we just defined.
+            
+            // Make the map show that region we just defined
             self.campusMap.setRegion(region, animated: true)
-            //the following code is to prevent a bug which affects the zooming of the map to theuser's location.
-            //We have to leave a little time after our initial setting of the map's location andspan,
-            //before we can start centering on the user's location, otherwise the map never zooms inbecause the
-            //intial zoom level and span are applied to the setCenter( ) method call, rather than our "requested" ones,
-            //once they have taken effect on the map.
-            //we setup a timer to set our boolean to true in 5 seconds.
+            
+            // The following code is to prevent a bug which affects the zooming of the map to theuser's location.
             _ = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(startUserTracking), userInfo: nil, repeats: false)
         }
         
@@ -49,8 +51,7 @@ class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegat
     }
     
     
-    //this method sets the startTrackingTheUser boolean class property to true. Once it's true,
-    //subsequent calls to didUpdateLocations will cause the map to centre on the user's location.
+    // This method sets the startTrackingTheUser boolean class property to true. Once it's true, subsequent calls to didUpdateLocations will cause the map to centre on the user's location.
     @objc func startUserTracking() {
         startTrackingTheUser = true
     }
