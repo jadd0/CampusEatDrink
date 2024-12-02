5f6bb59b3e2af8ed1185dbfa4b835bb775e9b4cc
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2024-12-02 12:33:59 +0000

    Initial Commit

---
+++ CampusEatDrink/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CampusEatDrink
+//
+//  Created by Jadd Al-Khabbaz on 02/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CampusEatDrinkTests/CampusEatDrinkTests.swift
@@ -0,0 +1,17 @@
+//
+//  CampusEatDrinkTests.swift
+//  CampusEatDrinkTests
+//
+//  Created by Jadd Al-Khabbaz on 02/12/2024.
+//
+
+import Testing
+@testable import CampusEatDrink
+
+struct CampusEatDrinkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
